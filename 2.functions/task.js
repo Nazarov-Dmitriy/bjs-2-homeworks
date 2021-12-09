1d3b1f1b011e99ad3f7df5a9be250849aca5e77d
Dmitriy <Difat1@inbox.ru>
2021-12-09 22:00:21 +0300

    corrected task.js function

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,66 +1,21 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
-}
-
-// Задание 2
-function worker(arr) {
-  let sum;
-
-  // Ваш код
-
-  return sum;
-}
-
-function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
-  return max;
-}
-
-// Задание 3
-function worker2(arr) {
-  // Ваш код
-}
-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
-
-// Задание 1
-function getArrayParams(arr) {
-  let min, max, sum, avg;
   min = Infinity;
   max = -Infinity;
   sum = 0;
 
-  function minValue(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      if (min > arr[i]) min = arr[i];
+  for (let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
     }
-  }
-
-  function maxValue(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      if (max < arr[i]) max = arr[i];
+    if (max < arr[i]) {
+      max = arr[i];
     }
+    sum += arr[i];
   }
-
-  function sumValue(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      sum += arr[i];
-    }
-    avg = +(sum / arr.length).toFixed(2);
-  }
-
-  minValue(arr);
-  sumValue(arr);
-  maxValue(arr);
-
+  avg = +(sum / arr.length).toFixed(2);
+  
   return {
     min: min,
     max: max,
@@ -69,7 +24,6 @@ function getArrayParams(arr) {
 }
 
 // Задание 2
-// nnnnjgij 
 
 
 function worker(arr) {
